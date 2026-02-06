c379518c63769f1365f1549c4ac9421b571ef842
cyzanas <cyzanas34@gmail.com>
2026-02-06 19:12:16 +0530

    Add files via upload

---
+++ if.js
@@ -0,0 +1,12 @@
+let age=20;
+
+if(age<18){
+    console.log("you are in teenage")
+}
+
+else if(age>18 && age<29){
+    console.log("you are not a teenager now")
+}
+else{
+    console.log("you are getting old")
+}
