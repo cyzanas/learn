68525d0429cc17f4dc797b307b092acc927f8437
cyzanas <cyzanas34@gmail.com>
2026-02-06 18:51:15 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,27 @@
+//object
+// let obj={
+//     name:"john",
+//     john_smith:"smith",
+// }
+// console.log(obj.name)
+
+// array
+
+//  let myArray=["lol","yeah"]
+//  console.log(myArray[0,1])
+
+
+//array methods
+
+let myArray=["A","C","B"]
+//array.push(3);
+//myArray.pop("B")
+//myArray.unshift("D")
+//myArray.shift()
+//myArray.splice(0,2)
+//let newArray = myArray.slice(1)
+//console.log(myArray.length);
+//console.log(newArray)
+// myArray.reverse()
+myArray.sort()
+console.log(myArray)
