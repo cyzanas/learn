36b99afd5c507b5c5247d01d82c5d4e28e888199
cyzanas <cyzanas34@gmail.com>
2026-02-07 22:42:28 +0530

    Add files via upload

---
+++ arrayFor.js
@@ -0,0 +1,4 @@
+let myArray=["john","doe","jacob"]
+for(let i=0;i<myArray.length;i++){
+    document.write(myArray[i] +"<br>");
+}
---
+++ objFor.js
@@ -0,0 +1,8 @@
+let myObj={
+    firstName:"john",
+    lastName:"jacob",
+}
+
+for(let x in myObj){
+    document.writeln(myObj[x],"<br>")
+}
